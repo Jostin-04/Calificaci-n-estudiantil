048ad5339c200d0ba086632b8f5b4fab57ad6d6f
Jostin Sierra Madrigal <ostinsierra100@gmail.com>
2026-01-31 19:45:42 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Notas3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.vtnCalcular = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.txtCompletivo = new System.Windows.Forms.TextBox();
+            this.txtExtraordinario = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(139, 57);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 0;
+            this.txtNota1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(139, 99);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 1;
+            this.txtNota2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(324, 57);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 2;
+            this.txtNota3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(324, 99);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 20);
+            this.txtNota4.TabIndex = 3;
+            this.txtNota4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(273, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nota 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(273, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nota 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(85, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(85, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nota 1";
+            // 
+            // vtnCalcular
+            // 
+            this.vtnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.vtnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vtnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vtnCalcular.ForeColor = System.Drawing.SystemColors.Info;
+            this.vtnCalcular.Location = new System.Drawing.Point(57, 309);
+            this.vtnCalcular.Name = "vtnCalcular";
+            this.vtnCalcular.Padding = new System.Windows.Forms.Padding(2);
+            this.vtnCalcular.Size = new System.Drawing.Size(75, 35);
+            this.vtnCalcular.TabIndex = 8;
+            this.vtnCalcular.Text = "Calcular";
+            this.vtnCalcular.UseVisualStyleBackColor = false;
+            this.vtnCalcular.Click += new System.EventHandler(this.vtnCalcular_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(88, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Promedio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Info;
+            this.label6.Location = new System.Drawing.Point(230, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Completivo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Info;
+            this.label7.Location = new System.Drawing.Point(369, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Extraordinario";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(67, 184);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtPromedio.TabIndex = 12;
+            this.txtPromedio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtCompletivo
+            // 
+            this.txtCompletivo.Enabled = false;
+            this.txtCompletivo.Location = new System.Drawing.Point(208, 184);
+            this.txtCompletivo.Name = "txtCompletivo";
+            this.txtCompletivo.Size = new System.Drawing.Size(100, 20);
+            this.txtCompletivo.TabIndex = 13;
+            this.txtCompletivo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtExtraordinario
+            // 
+            this.txtExtraordinario.Enabled = false;
+            this.txtExtraordinario.Location = new System.Drawing.Point(354, 184);
+            this.txtExtraordinario.Name = "txtExtraordinario";
+            this.txtExtraordinario.Size = new System.Drawing.Size(100, 20);
+            this.txtExtraordinario.TabIndex = 14;
+            this.txtExtraordinario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Info;
+            this.label8.Location = new System.Drawing.Point(88, 259);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Condicion:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(157, 256);
+            this.txtResultado.MaximumSize = new System.Drawing.Size(200, 20);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(200, 20);
+            this.txtResultado.TabIndex = 16;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnLimpiar.Location = new System.Drawing.Point(208, 309);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Padding = new System.Windows.Forms.Padding(2);
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 35);
+            this.btnLimpiar.TabIndex = 17;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnSalir.Location = new System.Drawing.Point(378, 309);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Padding = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Size = new System.Drawing.Size(75, 35);
+            this.btnSalir.TabIndex = 18;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(524, 360);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtExtraordinario);
+            this.Controls.Add(this.txtCompletivo);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.vtnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button vtnCalcular;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.TextBox txtCompletivo;
+        private System.Windows.Forms.TextBox txtExtraordinario;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notas3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vtnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtNota1.Text);
+            double n2 = double.Parse(txtNota2.Text);
+            double n3 = double.Parse(txtNota3.Text);
+            double n4 = double.Parse(txtNota4.Text);
+
+            double Promedio = (n1 + n2 + n3 + n4) / 4;
+            txtPromedio.Text = Promedio.ToString("0.00");
+
+            if (Promedio >= 70)
+            {
+                txtResultado.Text = "Aprobado";
+                txtCompletivo.Enabled = false;
+                txtExtraordinario.Enabled = false;
+                return;
+            }
+
+            txtCompletivo.Enabled = true;
+
+            if (txtCompletivo.Text == "")
+            {
+                MessageBox.Show("Ingrese la nota del completivo y presione Calcular otra vez");
+                return;
+            }
+
+            double NotaCompletivo;
+            if (!double.TryParse(txtCompletivo.Text, out NotaCompletivo))
+            {
+                MessageBox.Show("La nota del completivo no es válida");
+                return;
+            }
+
+            double totalCompletivo = (Promedio * 0.50) + (NotaCompletivo * 0.50);
+
+            if (totalCompletivo >= 70)
+            {
+                txtResultado.Text = "Aprobado con " + totalCompletivo+ " en completivo";
+                txtExtraordinario.Enabled = false;
+                return;
+            }
+
+            txtExtraordinario.Enabled = true;
+
+            if (txtExtraordinario.Text == "")
+            {
+                MessageBox.Show("Ingrese la nota del extraordinario y presione Calcular otra vez");
+                return;
+            }
+
+            double notaExtra;
+            if (!double.TryParse(txtExtraordinario.Text, out notaExtra))
+            {
+                MessageBox.Show("La nota del extraordinario no es válida");
+                return;
+            }
+
+            double totalExtraordinario = (Promedio * 0.30) + (notaExtra * 0.70);
+
+            if (totalExtraordinario >= 70)
+                txtResultado.Text = "Aprobado con " + totalExtraordinario + " en extraodinario";
+            else
+                txtResultado.Text = "Reprobado con "+ totalExtraordinario;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Clear();
+            txtNota2.Clear();
+            txtNota3.Clear();
+            txtNota4.Clear();
+            txtPromedio.Clear();
+            txtCompletivo.Clear();
+            txtExtraordinario.Clear();
+            txtResultado.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
